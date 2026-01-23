d27d2e7d6efb03c290c066f6e4213e6b27bebafd
Markus Schirp <mbj@schirp-dso.com>
2026-01-23 19:12:56 +0000

    Add collation identifier type

--- pg-client/src/identifier.rs
+++ pg-client/src/identifier.rs
@@ -336,6 +336,12 @@ define_identifier_type!(
 );
 
 define_identifier_type!(
+    /// A PostgreSQL collation name.
+    Collation,
+    collation
+);
+
+define_identifier_type!(
     /// A PostgreSQL database name.
     Database,
     database
