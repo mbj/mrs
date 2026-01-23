e8f513fa81b3db6a318f87b2c3cc3e34f4715b79
Markus Schirp <mbj@schirp-dso.com>
2026-01-23 19:30:59 +0000

    Add event trigger identifier type

--- pg-client/src/identifier.rs
+++ pg-client/src/identifier.rs
@@ -390,6 +390,12 @@ define_identifier_type!(
 );
 
 define_identifier_type!(
+    /// A PostgreSQL event trigger name.
+    EventTrigger,
+    event_trigger
+);
+
+define_identifier_type!(
     /// A PostgreSQL database name.
     Database,
     database
